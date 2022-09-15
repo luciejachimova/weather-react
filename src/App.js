d8b756a089ac34a927800e521ea33ecfe6cdb5f9
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-09-15 11:39:52 +0200

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lucie Jáchimová and is{" "}
+        <a
+          href="https://github.com/luciejachimova/weather-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-

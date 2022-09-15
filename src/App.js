c98ae9f99ef12e64a2c49a57b4f0776fe92dc463
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-09-15 11:19:38 +0200

    Removed unessary files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
+
 import Weather from "./Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Tokyo" />
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App">Weather App</div>;
 }
 
-export default App;
+
